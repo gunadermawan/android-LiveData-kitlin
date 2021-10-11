4cdf5b5e2143084d9596a9c5b27d6c605e80211d
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-10-11 19:48:34 +0700

    connect MainActivity.kt to MainViewModel

--- app/src/main/java/com/gunder/mylivedata/MainActivity.kt
+++ app/src/main/java/com/gunder/mylivedata/MainActivity.kt
@@ -2,10 +2,19 @@ package com.gunder.mylivedata
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.gunder.mylivedata.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    //    binding
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var mLiveDataTimerViewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mLiveDataTimerViewModel = ViewModelProvider(this)[MainViewModel::class.java]
     }
 }
