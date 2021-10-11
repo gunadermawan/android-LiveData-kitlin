71760d7efc8c7fd76fe769ef1ee1a3703fa93fa0
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-10-11 19:39:31 +0700

    add method to runing timmer in constructor class

--- app/src/main/java/com/gunder/mylivedata/MainViewModel.kt
+++ app/src/main/java/com/gunder/mylivedata/MainViewModel.kt
@@ -1,6 +1,22 @@
 package com.gunder.mylivedata
 
+import android.os.SystemClock
 import androidx.lifecycle.ViewModel
+import java.util.*
 
-class MainViewModel: ViewModel() {
+class MainViewModel : ViewModel() {
+    companion object {
+        private const val ONE_SECOND = 1000
+    }
+
+    private val mInitialTime = SystemClock.elapsedRealtime()
+
+    init {
+        val timer = Timer()
+        timer.scheduleAtFixedRate(object : TimerTask() {
+            override fun run() {
+                val newValue = (SystemClock.elapsedRealtime() - mInitialTime) / 1000
+            }
+        }, ONE_SECOND.toLong(), ONE_SECOND.toLong())
+    }
 }
