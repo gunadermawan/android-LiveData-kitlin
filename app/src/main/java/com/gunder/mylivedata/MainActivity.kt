dcd9fbcce6aedf3f69eea44f106ec6e2fc890cc6
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-10-11 21:00:40 +0700

    - mengganti root layout aplikasi - membuat method subscribe untk update data dari liveData

--- app/src/main/java/com/gunder/mylivedata/MainActivity.kt
+++ app/src/main/java/com/gunder/mylivedata/MainActivity.kt
@@ -2,6 +2,7 @@ package com.gunder.mylivedata
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.gunder.mylivedata.databinding.ActivityMainBinding
 
@@ -16,5 +17,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         mLiveDataTimerViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        subscribe()
+    }
+
+    private fun subscribe() {
+        val elapsedTimeObserver = Observer<Long?> { aLong ->
+            val newText = this@MainActivity.resources.getString(R.string.seconds, aLong)
+            binding.timerTextview.text = newText
+        }
+        mLiveDataTimerViewModel.geteElapsedTime().observe(this, elapsedTimeObserver)
     }
 }
